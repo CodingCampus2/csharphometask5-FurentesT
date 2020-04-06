5773a41919d7e7518015673e1d40932c99ba32cd
Taras Pobyvanets <furentes.t@gmail.com>
2020-04-06 10:03:46 +0300

    Homework done

--- Program.cs
+++ Program.cs
@@ -3,6 +3,27 @@ using CodingCampusCSharpHomework;
 
 namespace HomeworkTemplate
 {
+    class SuperVirus : Task5.Virus
+    {
+        public SuperVirus() :
+            base(false)
+        {
+            DateTimeOfCreation = DateTime.Now;
+        }
+        public override float KillProbability {
+            get { return killProbability; }
+            set { killProbability = MathF.Min(value * 2, 1.0f); }
+        }
+        public override string Name { 
+            get { return name; } 
+            set { name = string.Format("{0}supervirus", value.Substring(0, value.LastIndexOf('v'))); } 
+        }
+        public override string VictimName { get; set; }
+        public override DateTime DateTimeOfCreation { get; }
+
+        private float killProbability;
+        private string name;
+    }
     class Program
     {
         static void Main(string[] args)
